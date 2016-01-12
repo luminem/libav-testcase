b8536aaa01b9a8ac5a2a39965687e384275417cb
Luca Barbato <lu_zero@gentoo.org>
2016-01-12 21:04:37 +0100

    Make sure the AVFormatContext is fully set
    And also make sure that the codec parameters needed are set as well.

--- src/main/java/it/luminem/LibavFrameRecorder.java
+++ src/main/java/it/luminem/LibavFrameRecorder.java
@@ -9,6 +9,7 @@ import static org.bytedeco.javacpp.avutil.*;
 public class LibavFrameRecorder {
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        int ret;
         String filename = "output.mpeg";
         Loader.load(org.bytedeco.javacpp.avutil.class);
         Loader.load(org.bytedeco.javacpp.avcodec.class);
@@ -17,19 +18,28 @@ public class LibavFrameRecorder {
         AVOutputFormat format = av_guess_format("mpeg", null, null);
         AVFormatContext oc = avformat_alloc_context();
         oc.filename().putString(filename);
-        System.out.println("OC " + oc);
+        oc.oformat(format);
+
         AVStream st = avformat_new_stream(oc, codec);
-        st.codec().codec_id(codec.id());
-        st.codec().codec_type(AVMEDIA_TYPE_VIDEO);
-        st.codec().pix_fmt(AV_PIX_FMT_YUV420P);
+        AVCodecContext c = st.codec();
+        c.codec_id(codec.id());
+        c.codec_type(AVMEDIA_TYPE_VIDEO);
+        c.pix_fmt(AV_PIX_FMT_YUV420P);
+
         AVRational frame_rate = new AVRational();
         frame_rate.num(30);
         frame_rate.den(1);
         st.time_base(av_inv_q(frame_rate));
-        st.codec().time_base(av_inv_q(frame_rate));
-        avcodec_open2(st.codec(), null, (PointerPointer)null);
+        c.time_base(av_inv_q(frame_rate));
+        c.width(640);
+        c.height(480);
+        ret = avcodec_open2(c, codec, (PointerPointer)null);
+        if (ret < 0) {
+            throw new RuntimeException("avcodec_open2() error " + ret);
+        }
 
-        int ret;
+        av_dump_format(oc, 0, filename, 1);
+
         AVIOContext pb = new AVIOContext(null);
         if ((ret = avio_open(pb, "output.mpeg", AVIO_FLAG_WRITE)) < 0) {
             //release();
@@ -37,7 +47,10 @@ public class LibavFrameRecorder {
         }
         oc.pb(pb);
 
-        avformat_write_header(oc, new AVDictionary());
+        ret = avformat_write_header(oc, (PointerPointer)null);
+        if (ret < 0) {
+            throw new RuntimeException("avformat_write_header() error " + ret);
+        }
     }
 
 }
