6356235930d4751c26d4c6fb6e9856fe90ef7166
Federico Tomassetti <federico@tomassetti.me>
2016-01-12 17:39:28 +0100

    wip

--- src/main/java/it/luminem/LibavFrameRecorder.java
+++ src/main/java/it/luminem/LibavFrameRecorder.java
@@ -2,26 +2,42 @@ package it.luminem;
 
 import org.bytedeco.javacpp.*;
 
-import java.lang.reflect.Field;
-
 import static org.bytedeco.javacpp.avcodec.*;
 import static org.bytedeco.javacpp.avformat.*;
+import static org.bytedeco.javacpp.avutil.*;
 
 public class LibavFrameRecorder {
 
     public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
-        System.setProperty("java.library.path", System.getProperty("java.library.path")
-                + ":/home/federico/repos/javacpp-presets/libav/cppbuild/linux-x86_64/lib"
-                + ":/home/federico/repos/javacpp-presets/libav/target/classes/org/bytedeco/javacpp/linux-x86_64");
-        final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
-        sysPathsField.setAccessible(true);
-        sysPathsField.set(null, null);
-        System.out.println(System.getProperty("java.library.path"));
+        String filename = "output.mpeg";
         Loader.load(org.bytedeco.javacpp.avutil.class);
         Loader.load(org.bytedeco.javacpp.avcodec.class);
         av_register_all();
-        AVCodec codec = avcodec_find_decoder_by_name("mpeg2video");
-        System.out.println("GOT CODEC: " + codec);
+        AVCodec codec = avcodec_find_encoder_by_name("mpeg2video");
+        AVOutputFormat format = av_guess_format("mpeg", null, null);
+        AVFormatContext oc = avformat_alloc_context();
+        oc.filename().putString(filename);
+        System.out.println("OC " + oc);
+        AVStream st = avformat_new_stream(oc, codec);
+        st.codec().codec_id(codec.id());
+        st.codec().codec_type(AVMEDIA_TYPE_VIDEO);
+        st.codec().pix_fmt(AV_PIX_FMT_YUV420P);
+        AVRational frame_rate = new AVRational();
+        frame_rate.num(30);
+        frame_rate.den(1);
+        st.time_base(av_inv_q(frame_rate));
+        st.codec().time_base(av_inv_q(frame_rate));
+        avcodec_open2(st.codec(), null, (PointerPointer)null);
+
+        int ret;
+        AVIOContext pb = new AVIOContext(null);
+        if ((ret = avio_open(pb, "output.mpeg", AVIO_FLAG_WRITE)) < 0) {
+            //release();
+            throw new RuntimeException("avio_open error() error " + ret + ": Could not open '" + filename + "'");
+        }
+        oc.pb(pb);
+
+        avformat_write_header(oc, new AVDictionary());
     }
 
 }
